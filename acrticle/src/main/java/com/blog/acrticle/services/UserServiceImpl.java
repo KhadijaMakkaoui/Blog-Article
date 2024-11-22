1f6e87b1b6fe82313b7fd4e97661e69d27fc55e8
KhadijaMakkaoui <makkaoui.khadija44@gmail.com>
2024-11-22 12:26:36 +0100

    implement register user method

--- acrticle/src/main/java/com/blog/acrticle/services/UserServiceImpl.java
+++ acrticle/src/main/java/com/blog/acrticle/services/UserServiceImpl.java
@@ -1,15 +1,20 @@
 package com.blog.acrticle.services;
 
 import com.blog.acrticle.dtos.ApiResponseDto;
+import com.blog.acrticle.dtos.ApiResponseStatus;
 import com.blog.acrticle.dtos.UserRegistrationDTO;
 import com.blog.acrticle.exceptions.UserAlreadyExistsException;
 import com.blog.acrticle.exceptions.UserNotFoundException;
 import com.blog.acrticle.exceptions.UserServiceLogicException;
+import com.blog.acrticle.models.User;
 import com.blog.acrticle.repositories.UserRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
 
 @Component
 @Slf4j
@@ -20,7 +25,28 @@ public class UserServiceImpl implements UserService{
 
     @Override
     public ResponseEntity<ApiResponseDto<?>> registerUser(UserRegistrationDTO newUserDetails) throws UserAlreadyExistsException, UserServiceLogicException {
-        return null;
+        try {
+            if (userRepository.findByEmail(newUserDetails.getEmail())!=null) {
+                throw new UserAlreadyExistsException("Registration failed: User already exists with email " + newUserDetails.getEmail());
+            }
+            if (userRepository.findByUserName(newUserDetails.getUserName())!=null) {
+                throw new UserAlreadyExistsException("Registration failed: User already exists with username " + newUserDetails.getUserName());
+            }
+            User newUser=new User() ;
+            newUser.setEmail(newUserDetails.getEmail());
+            newUser.setUsername(newUserDetails.getUserName());
+            newUser.setPhone(newUserDetails.getPhone());
+            newUser.setRegDateAndTime(LocalDateTime.now());
+            userRepository.save(newUser);
+            return ResponseEntity.status(HttpStatus.CREATED)
+                    .body(new ApiResponseDto<>(ApiResponseStatus.SUCCESS.name(),
+                            "New user account has been successfully created!"));
+        }catch (UserAlreadyExistsException e){
+            throw new UserAlreadyExistsException(e.getMessage());
+        }catch (Exception e){
+            log.error("Failed to create new user account: " + e.getMessage());
+            throw new UserServiceLogicException();
+        }
     }
 
     @Override
