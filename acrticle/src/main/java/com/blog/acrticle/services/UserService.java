32ecd3c855e68e6e133bcc7a14daca155421a8cb
KhadijaMakkaoui <makkaoui.khadija44@gmail.com>
2024-11-22 11:46:03 +0100

    setup User service Interface

---
+++ acrticle/src/main/java/com/blog/acrticle/services/UserService.java
@@ -0,0 +1,22 @@
+package com.blog.acrticle.services;
+
+import com.blog.acrticle.dtos.ApiResponseDto;
+import com.blog.acrticle.dtos.UserRegistrationDTO;
+import com.blog.acrticle.exceptions.UserAlreadyExistsException;
+import com.blog.acrticle.exceptions.UserNotFoundException;
+import com.blog.acrticle.exceptions.UserServiceLogicException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+@Service
+public interface UserService {
+    ResponseEntity<ApiResponseDto<?>> registerUser(UserRegistrationDTO newUserDetails)
+        throws UserAlreadyExistsException, UserServiceLogicException;
+    ResponseEntity<ApiResponseDto<?>> getAllUsers()
+        throws UserNotFoundException, UserServiceLogicException;
+
+    ResponseEntity<ApiResponseDto<?>> deleteUser(int id)
+        throws UserNotFoundException, UserServiceLogicException;
+
+
+}
