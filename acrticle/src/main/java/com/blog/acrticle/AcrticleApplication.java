7417e83f07c23f4876b2659067aa730bee932732
KhadijaMakkaoui <makkaoui.khadija44@gmail.com>
2024-11-22 09:40:04 +0100

    project setup

---
+++ acrticle/src/main/java/com/blog/acrticle/AcrticleApplication.java
@@ -0,0 +1,13 @@
+package com.blog.acrticle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcrticleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcrticleApplication.class, args);
+	}
+
+}
---
+++ acrticle/src/test/java/com/blog/acrticle/AcrticleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blog.acrticle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcrticleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
