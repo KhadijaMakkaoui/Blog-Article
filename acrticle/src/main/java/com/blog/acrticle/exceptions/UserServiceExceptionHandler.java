356ad681c341b973d820280152c305b287acc4c1
KhadijaMakkaoui <makkaoui.khadija44@gmail.com>
2024-11-26 09:43:42 +0100

    Implement method argument not valid exception

--- acrticle/src/main/java/com/blog/acrticle/exceptions/UserServiceExceptionHandler.java
+++ acrticle/src/main/java/com/blog/acrticle/exceptions/UserServiceExceptionHandler.java
@@ -24,11 +24,20 @@ public class UserServiceExceptionHandler {
     public ResponseEntity<ApiResponseDto<?>> UserAlreadyExistsExceptionHandler(UserAlreadyExistsException exception) {
         return ResponseEntity.status(HttpStatus.CONFLICT).body(new ApiResponseDto<>(ApiResponseStatus.FAILURE.name(), exception.getMessage()));
     }
+
     @ExceptionHandler(value = UserServiceLogicException.class)
     public ResponseEntity<ApiResponseDto<?>> UserServiceLogicExceptionHandler(UserServiceLogicException exception) {
         return ResponseEntity.badRequest().body(new ApiResponseDto<>(ApiResponseStatus.FAILURE.name(), exception.getMessage()));
     }
 
-
+    @ExceptionHandler(value = MethodArgumentNotValidException.class)
+    public ResponseEntity<ApiResponseDto<?>> MethodArgumentNotValidExceptionHandler(MethodArgumentNotValidException exception){
+        List<String> errorMessage = new ArrayList<>();
+        exception.getBindingResult().getFieldErrors()
+                .forEach(error ->{
+                    errorMessage.add(error.getDefaultMessage());
+                });
+        return ResponseEntity.badRequest().body(new ApiResponseDto<>(ApiResponseStatus.FAILURE.name(), errorMessage.toString()));
+    }
 
 }
